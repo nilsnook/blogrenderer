6a7caa0da0f70247833828f5fa67de2e8ca0d3bf
Nikhil Sahukar <nsahukar@gmail.com>
2023-02-02 12:04:56 +0530

    Added blog posts' description and tags in testing its HTML rendering

--- blogrenderer.go
+++ blogrenderer.go
@@ -13,6 +13,26 @@ type Post struct {
 }
 
 func Render(w io.Writer, p Post) error {
-	fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	// Title and Description
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>\n", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+	// Tags
+	_, err = fmt.Fprint(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+	for _, t := range p.Tags {
+		_, err = fmt.Fprintf(w, "<li>%s</li>", t)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprint(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
